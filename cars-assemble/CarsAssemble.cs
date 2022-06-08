596c4f80522073aac32340261e70879afa66a4f4
rwclutterbuck <rwclutterbuck@outlook.com>
2022-06-08 11:27:48 +0100

    Initial solution to problem

--- cars-assemble/CarsAssemble.cs
+++ cars-assemble/CarsAssemble.cs
@@ -4,16 +4,36 @@ static class AssemblyLine
 {
     public static double SuccessRate(int speed)
     {
-        throw new NotImplementedException("Please implement the (static) AssemblyLine.SuccessRate() method");
+        double rate;
+        if (speed == 0) 
+        {
+            rate = 0;
+        }
+        else if (1 <= speed && speed <= 4) 
+        {
+            rate = 1;
+        }
+        else if (5 <= speed && speed <= 8)
+        {
+            rate = 0.9;
+        }
+        else if (speed == 9)
+        {
+            rate = 0.8;
+        }
+        else
+        {
+            rate = 0.77;
+        }
+        return rate;
     }
     
     public static double ProductionRatePerHour(int speed)
     {
-        throw new NotImplementedException("Please implement the (static) AssemblyLine.ProductionRatePerHour() method");
+        int productionRate = 221;
+        return SuccessRate(speed) * speed * productionRate;
     }
 
-    public static int WorkingItemsPerMinute(int speed)
-    {
-        throw new NotImplementedException("Please implement the (static) AssemblyLine.WorkingItemsPerMinute() method");
-    }
+    public static int WorkingItemsPerMinute(int speed) =>
+        (int)ProductionRatePerHour(speed) / 60;
 }
